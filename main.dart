f1d10223db3f85cccccff9447d6cf38f72254fb6
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-20 10:26:38 +0700

    ✨

--- main.dart
+++ main.dart
@@ -35,6 +35,33 @@ void main() {
   var score = 85;
 
   print('Your grade => ${calculateScore(score)}');
+  // doWhile();
+
+  funcbreak();
+}
+
+void funcbreak() {
+  for (int i = 11; i < 20; i++) {
+    if (i % 11 == 0) {
+      continue;
+    }
+    stdout.write('$i ');
+    if (i % 5 == 0) {
+      break;
+    }
+  }
+}
+
+void doWhile() {
+  int i = 1;
+  // while (i < 100) {
+  //   print(i);
+  //   i++;
+  // }
+  do {
+    print(i);
+    i++;
+  } while (i < 10);
 }
 
 void birthday(String name, int age) {
@@ -94,5 +121,81 @@ String calculateScore(num score) {
     return 'D';
   } else {
     return 'E';
+  }
+}
+
+void prime() {
+  var primeNumbers = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    73,
+    79,
+    83,
+    89,
+    97
+  ];
+
+  var searchNumber = 13;
+  print('Prime numbers among 1-100: $searchNumber');
+
+  for (int i = 0; i < primeNumbers.length; i++) {
+    if (searchNumber == primeNumbers[i]) {
+      print('$searchNumber is a prime number to-${i + 1}');
+      break;
+    }
+    print('$searchNumber != ${primeNumbers[i]}');
+  }
+}
+
+void loopContinue() {
+  for (int i = 1; i <= 10; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
+
+void switchCase() {
+  final firstNumber = 13;
+  final secondNumber = 18;
+  final operator = "+";
+
+  switch (operator) {
+    case '+':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber + secondNumber}');
+      break;
+    case '-':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber - secondNumber}');
+      break;
+    case '*':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber * secondNumber}');
+      break;
+    case '/':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber / secondNumber}');
+      break;
+    default:
+      print('Operator tidak ditemukan');
   }
 }
