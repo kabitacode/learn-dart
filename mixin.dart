efcb056fbba48fb222ed7009747fc8b5d47c6a81
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-24 13:54:02 +0700

    mixin

---
+++ mixin.dart
@@ -0,0 +1,28 @@
+void main() {
+  var a7x = Musician();
+  a7x.perform();
+}
+
+abstract class Performer {
+  void perform();
+}
+
+mixin Dancer implements Performer {
+  @override
+  void perform() {
+    print('Dancing');
+  }
+}
+
+mixin Singer implements Performer {
+  @override
+  void perform() {
+    print('Singing');
+  }
+}
+
+class Musician extends Performer with Dancer, Singer {
+  void showTime() {
+    perform();
+  }
+}
